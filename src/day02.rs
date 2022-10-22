b425f36cc509628d5d8617ef9875d081f3c56f83
Richard Clamp <richardc@unixbeard.net>
2022-10-22 18:10:13 +0100

    Day 2 Part 2: Find the permutation where one_different holds
    one_different takes two &str and then if they differ by one character,
    we return a String of the common characters.
    
    We use slices to make this nice and fast, and permutations from
    itertools to generate the comparisons.

--- src/day02.rs
+++ src/day02.rs
@@ -18,3 +18,33 @@ fn solve(input: &str) -> usize {
 fn test_solve() {
     assert_eq!(solve(include_str!("day02_example.txt")), 12)
 }
+
+fn one_different(a: &str, b: &str) -> Option<String> {
+    let a = a.as_bytes();
+    let b = b.as_bytes();
+    for i in 0..a.len() {
+        if a[..i] == b[..i] && a[i + 1..] == b[i + 1..] {
+            return Some(format!(
+                "{}{}",
+                std::str::from_utf8(&a[..i]).unwrap(),
+                std::str::from_utf8(&a[i + 1..]).unwrap()
+            ));
+        }
+    }
+    None
+}
+
+#[aoc(day2, part2)]
+fn solve2(input: &str) -> String {
+    input
+        .lines()
+        .permutations(2)
+        .find_map(|v| one_different(v[0], v[1]))
+        .unwrap()
+}
+
+#[cfg(test)]
+#[test]
+fn test_solve2() {
+    assert_eq!(solve2(include_str!("day02_example2.txt")), "fgij")
+}
