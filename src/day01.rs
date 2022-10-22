6dbc8981e90fd5a88f45714dd33eaddfab2e69de
Richard Clamp <richardc@unixbeard.net>
2022-10-22 16:51:34 +0100

    Day 1 Parts 1 and 2.
    Part one is a very simple 'you can load a file and add it up, right?'
    
    Part two is still pretty straightforward, loop over the input tracking
    the current frequency and if we've seen it (HashSet.insert returns false
    when inserting a duplicate) return that.
    
    Adds in the test_case crate, for terse table-based testing.

--- src/day01.rs
+++ src/day01.rs
@@ -1,4 +1,35 @@
+use std::collections::HashSet;
+
+#[aoc_generator(day1)]
+fn generate(s: &str) -> Vec<i32> {
+    s.lines().map(|l| l.parse().unwrap()).collect()
+}
+
 #[aoc(day1, part1)]
-fn solve(_: &str) -> usize {
-    return 0;
+fn solve(input: &[i32]) -> i32 {
+    input.iter().sum()
+}
+
+#[aoc(day1, part2)]
+fn solve2(input: &[i32]) -> i32 {
+    let mut seen = HashSet::new();
+    let mut freq = 0;
+    seen.insert(freq);
+    loop {
+        for reading in input {
+            freq += reading;
+            if !seen.insert(freq) {
+                return freq;
+            }
+        }
+    }
+}
+
+#[test_case(&[1, -1] => 0)]
+#[test_case(&[3, 3, 4, -2, -4] => 10)]
+#[test_case(&[-6, 3, 8, 5, -6] => 5)]
+#[test_case(&[7, 7, -2, -7, -4] => 14)]
+#[cfg(test)]
+fn _solve2(input: &[i32]) -> i32 {
+    solve2(input)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,10 @@ extern crate aoc_runner_derive;
 #[macro_use]
 extern crate pretty_assertions;
 
+#[cfg(test)]
+#[macro_use]
+extern crate test_case;
+
 mod day01;
 
 aoc_lib! { year = 2018 }
