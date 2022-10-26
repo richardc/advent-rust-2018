42e9c9be7105a9ae9b1b2974a0259529d8322056
Richard Clamp <richardc@unixbeard.net>
2022-10-26 12:07:25 +0100

    Day 11 Part 2: Real brute force
    We chach all the sizes from 1..299; and that's not smart.

--- src/day11.rs
+++ src/day11.rs
@@ -4,16 +4,45 @@ use itertools::iproduct;
 use itertools::Itertools;
 use ndarray::prelude::*;
 
-fn largest_cell_location(serial: usize) -> (usize, usize) {
+fn largest_3cell_location(serial: usize) -> (usize, usize) {
     let grid = make_grid(serial);
-    let sizes: HashMap<(usize, usize), i32> = iproduct!(0..300 - 3, 0..300 - 3)
-        .map(|(x, y)| ((x + 1, y + 1), grid.slice(s![x..x + 3, y..y + 3]).sum()))
+    let ((x, y), _) = largest_cell_sized(&grid, 3);
+    (x, y)
+}
+
+fn largest_cell_location(serial: usize) -> (usize, usize, usize) {
+    let grid = make_grid(serial);
+    let (size, ((x, y), _power)) = (1..300)
+        .into_iter()
+        .map(|size| (size, largest_cell_sized(&grid, size)))
+        .sorted_by_key(|&(_, (_, power))| power)
+        .rev()
+        .next()
+        .unwrap();
+    (x, y, size)
+}
+
+#[test_case(8 => (3,5,9))]
+#[test_case(18 => (90,269,16))]
+#[test_case(42 => (232,251,12))]
+#[cfg(test)]
+fn test_largest_cell_location(serial: usize) -> (usize, usize, usize) {
+    largest_cell_location(serial)
+}
+
+fn largest_cell_sized(grid: &Array2<i32>, size: usize) -> ((usize, usize), i32) {
+    let sizes: HashMap<(usize, usize), i32> = iproduct!(0..300 - size, 0..300 - size)
+        .map(|(x, y)| {
+            (
+                (x + 1, y + 1),
+                grid.slice(s![x..x + size, y..y + size]).sum(),
+            )
+        })
         .collect();
 
-    *sizes
-        .iter()
+    sizes
+        .into_iter()
         .sorted_by_key(|&(_, v)| v)
-        .map(|(k, _)| k)
         .rev()
         .next()
         .unwrap()
@@ -44,6 +73,12 @@ fn _cell_power(x: usize, y: usize, serial: usize) -> i32 {
 
 #[aoc(day11, part1)]
 fn solve(s: &str) -> String {
-    let (x, y) = largest_cell_location(s.parse().unwrap());
+    let (x, y) = largest_3cell_location(s.parse().unwrap());
     format!("{},{}", x, y)
+}
+
+#[aoc(day11, part2)]
+fn solve2(s: &str) -> String {
+    let (x, y, size) = largest_cell_location(s.parse().unwrap());
+    format!("{},{},{}", x, y, size)
 }
