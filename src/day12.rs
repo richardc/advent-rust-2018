cfb198c8e42f2f8c85056f3b51bdd267a1cc0ac8
Richard Clamp <richardc@unixbeard.net>
2022-10-27 12:32:08 +0100

    Day 12 Part 1: 20 iterations of an automaton
    Quite a pleasing pattern from this.
    
    Next generation is very strightforward, match the window to generate the
    next point.  Each trip round we grow each end by 1 pot, so track number
    of generations and use that in generating the score.

---
+++ src/day12.rs
@@ -0,0 +1,94 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
+
+#[derive(Clone)]
+struct Plants {
+    steps: usize,
+    pots: Vec<bool>,
+    rules: HashMap<[bool; 5], bool>,
+}
+
+impl std::fmt::Debug for Plants {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            String::from_iter(self.pots.iter().map(|&p| if p { '#' } else { '.' }))
+        )
+    }
+}
+
+impl std::str::FromStr for Plants {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut lines = s.lines();
+        let start = lines.next().unwrap();
+        let (_, pots) = start.split_once(": ").unwrap();
+        let pots = pots.chars().map(|c| c == '#').collect();
+        let rules: HashMap<[bool; 5], bool> = lines
+            .skip(1)
+            .map(|l| {
+                let (pattern, state) = l.split_once(" => ").unwrap();
+                (
+                    pattern
+                        .chars()
+                        .map(|c| c == '#')
+                        .collect_vec()
+                        .try_into()
+                        .unwrap(),
+                    state.chars().next().unwrap() == '#',
+                )
+            })
+            .collect();
+
+        let steps = 0;
+        Ok(Plants { steps, pots, rules })
+    }
+}
+
+impl Plants {
+    fn step(&mut self) {
+        let pots = [&[false; 3][..], &self.pots, &[false; 3][..]].concat();
+        let next = pots
+            .windows(5)
+            .map(|w| match self.rules.get(w) {
+                Some(&x) => x,
+                None => false,
+            })
+            .collect();
+        self.steps += 1;
+        self.pots = next;
+    }
+
+    fn score(&self) -> i64 {
+        self.pots
+            .iter()
+            .enumerate()
+            .map(|(i, &p)| if p { i as i64 - self.steps as i64 } else { 0 })
+            .sum()
+    }
+}
+
+#[aoc_generator(day12)]
+fn generate(input: &str) -> Plants {
+    input.parse().unwrap()
+}
+
+#[aoc(day12, part1)]
+fn solve(plants: &Plants) -> i64 {
+    let mut plants = plants.clone();
+    // println!("{:?}", plants);
+    for _ in 0..20 {
+        plants.step();
+        // println!("{:?}", plants);
+    }
+    plants.score()
+}
+
+#[cfg(test)]
+#[test]
+fn test_solve() {
+    assert_eq!(solve(&generate(include_str!("day12_example.txt"))), 325);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -20,5 +20,6 @@ mod day08;
 mod day09;
 mod day10;
 mod day11;
+mod day12;
 
 aoc_lib! { year = 2018 }
