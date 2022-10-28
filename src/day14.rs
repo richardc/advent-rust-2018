937b2463b0a345972a808b3f3ae108b40e487b51
Richard Clamp <richardc@unixbeard.net>
2022-10-28 13:43:19 +0100

    Day 14 Part 2: find_runs
    It's the same algorithm as before, but we must check the last few bytes
    to see if they match the

--- src/day14.rs
+++ src/day14.rs
@@ -63,3 +63,57 @@ fn score_next_ten(count: usize) -> String {
 fn _next_ten(count: usize) -> String {
     score_next_ten(count)
 }
+
+fn find_run(needle: &[u8]) -> usize {
+    let mut digits = vec![3_u8, 7];
+    let mut one = 0;
+    let mut two = 1;
+
+    loop {
+        let sum = digits[one as usize] + digits[two as usize];
+        digits.append(
+            &mut format!("{sum}")
+                .chars()
+                .map(|c| c.to_digit(10).unwrap() as u8)
+                .collect_vec(),
+        );
+
+        one += 1 + digits[one] as usize;
+        one %= digits.len();
+        two += 1 + digits[two] as usize;
+        two %= digits.len();
+
+        if digits.len() > needle.len() {
+            for start in
+                (digits.len() - needle.len()).saturating_sub(2)..digits.len() - needle.len()
+            {
+                if &digits[start..start + needle.len()] == needle {
+                    return start;
+                }
+            }
+        }
+    }
+}
+
+#[test_case("01245" => 5)]
+#[test_case("51589" => 9)]
+#[test_case("92510" => 18)]
+#[test_case("59414" => 2018)]
+#[cfg(test)]
+fn _find_run(s: &str) -> usize {
+    find_run(
+        &s.chars()
+            .map(|c| c.to_digit(10).unwrap() as u8)
+            .collect_vec(),
+    )
+}
+
+#[aoc(day14, part2)]
+fn solve2(input: &str) -> usize {
+    find_run(
+        &input
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as u8)
+            .collect_vec(),
+    )
+}
