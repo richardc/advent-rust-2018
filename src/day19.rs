1f4e11b76930b3456d3a1fdf94cd580cc721f156
Richard Clamp <richardc@unixbeard.net>
2022-11-02 16:57:42 +0000

    Day 19 Part 2: Another optimised sieve
    Like 2017 day 23 part 2, we're expected to either write an optimising
    compiler, or reconstruct the algorithm so we can implement it.
    
    This is the latter, but I looked over at the reddit to find out what
    the algorithm we're applying should be.

--- src/day19.rs
+++ src/day19.rs
@@ -192,3 +192,21 @@ fn solve(cpu: &Cpu) -> usize {
 fn test_solve() {
     assert_eq!(solve(&generate(include_str!("day19_example.txt"))), 6)
 }
+
+#[aoc(day19, part2)]
+fn solve2(cpu: &Cpu) -> usize {
+    let mut cpu = (*cpu).clone();
+    cpu.registers[0] = 1;
+    while cpu.registers[cpu.pc_reg] != 1 {
+        cpu.apply();
+    }
+
+    let seed = *cpu.registers.iter().max().unwrap();
+    let mut total = 0;
+    for i in 1..=seed {
+        if seed % i == 0 {
+            total += i;
+        }
+    }
+    total
+}
