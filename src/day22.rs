dd8583cc37d20a2014bdf9eaaf193a28882d01d5
Richard Clamp <richardc@unixbeard.net>
2022-11-13 10:58:29 +0000

    Day 22 Part 2: Calculate the shortest path
    Pretty standard, but rather than a 2D map, it's got the added dimension
    of tool.  We can maybe tidy this down a little more, say making all the
    7-cost equipment changes then using a filter with legal_tool.

--- src/day22.rs
+++ src/day22.rs
@@ -1,6 +1,20 @@
 use std::collections::HashMap;
 
 use itertools::{iproduct, Itertools};
+use pathfinding::prelude::dijkstra;
+
+enum Terrain {
+    Rocky,
+    Wet,
+    Narrow,
+}
+
+#[derive(Clone, Copy, Eq, PartialEq, Hash)]
+enum Tool {
+    None,
+    Torch,
+    Gear,
+}
 
 #[derive(Default, Clone)]
 struct Cave {
@@ -52,10 +66,77 @@ impl Cave {
         self.erosion(x, y) % 3
     }
 
+    fn terrain(&mut self, x: usize, y: usize) -> Terrain {
+        use Terrain::*;
+        match self.region(x, y) {
+            0 => Rocky,
+            1 => Wet,
+            2 => Narrow,
+            _ => unreachable!(),
+        }
+    }
+
     fn risk_level(&mut self) -> usize {
         iproduct!(0..=self.target.0, 0..=self.target.1)
             .map(|(x, y)| self.region(x, y))
             .sum()
+    }
+
+    fn legal_tool(terrain: Terrain, tool: Tool) -> bool {
+        use Terrain::*;
+        use Tool::*;
+        matches!(
+            (terrain, tool),
+            (Rocky, Torch)
+                | (Rocky, Gear)
+                | (Wet, None)
+                | (Wet, Gear)
+                | (Narrow, None)
+                | (Narrow, Torch)
+        )
+    }
+
+    fn neighbours(&mut self, x: usize, y: usize, tool: Tool) -> Vec<((usize, usize, Tool), usize)> {
+        let terrain = self.terrain(x, y);
+        let mut neighbours = vec![];
+        use Terrain::*;
+        use Tool::*;
+        // swap tool
+        match (terrain, tool) {
+            (Rocky, Torch) => neighbours.push(((x, y, Gear), 7)),
+            (Rocky, Gear) => neighbours.push(((x, y, Torch), 7)),
+            (Wet, None) => neighbours.push(((x, y, Gear), 7)),
+            (Wet, Gear) => neighbours.push(((x, y, None), 7)),
+            (Narrow, None) => neighbours.push(((x, y, Torch), 7)),
+            (Narrow, Torch) => neighbours.push(((x, y, None), 7)),
+            (_, _) => (),
+        }
+
+        // given current tool
+        if x > 0 && Self::legal_tool(self.terrain(x - 1, y), tool) {
+            neighbours.push(((x - 1, y, tool), 1))
+        }
+        if Self::legal_tool(self.terrain(x + 1, y), tool) {
+            neighbours.push(((x + 1, y, tool), 1))
+        }
+        if y > 0 && Self::legal_tool(self.terrain(x, y - 1), tool) {
+            neighbours.push(((x, y - 1, tool), 1))
+        }
+        if Self::legal_tool(self.terrain(x, y + 1), tool) {
+            neighbours.push(((x, y + 1, tool), 1))
+        }
+        neighbours
+    }
+
+    fn rescue_time(&mut self) -> usize {
+        let target = (self.target.0, self.target.1, Tool::Torch);
+        let (_path, cost) = dijkstra(
+            &(0, 0, Tool::Torch),
+            |&(x, y, tool)| self.neighbours(x, y, tool),
+            |&node| target == node,
+        )
+        .unwrap();
+        cost
     }
 }
 
@@ -73,4 +154,15 @@ fn solve(cave: &Cave) -> usize {
 #[test]
 fn test_solve() {
     assert_eq!(solve(&generate(include_str!("day22_example.txt"))), 114)
+}
+
+#[aoc(day22, part2)]
+fn solve2(cave: &Cave) -> usize {
+    (*cave).clone().rescue_time()
+}
+
+#[cfg(test)]
+#[test]
+fn test_solve2() {
+    assert_eq!(solve2(&generate(include_str!("day22_example.txt"))), 45)
 }
